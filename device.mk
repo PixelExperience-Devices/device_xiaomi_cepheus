e24d19e1feef864ff5d24ddf09a32bb08b91f4d4
Abodunrinwa Toki <toki@google.com>
2017-08-16 21:32:06 +0100

    cepheus: Remove textclassifier models
    This reverts commit d8036f0b8fbe65eff967dc90f623d04350410d0b.
    
    ConfigUpdateInstallReceiver will be used to push an updatable
    model to the devices.
    
    Test: Checked that a new build does not bundle any of the bundle1
    models (i.e. en, es, de, fr) by default.
    
    Bug: 64746048
    Change-Id: I507aa8d5911da8ae27f951fe566bb86c1a10238b

--- device.mk
+++ device.mk
@@ -425,10 +425,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
