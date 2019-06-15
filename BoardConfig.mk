6aa3867c7314b938311db1765373844164ef6177
Akash <akashniki@gmail.com>
2019-06-15 14:48:30 +0000

    cepheus: Add Ignore neverallow flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
