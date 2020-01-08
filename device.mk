5183adb2c7f8a48231986eac0c3908f02d191e26
Dyneteve <dyneteve@pixelexperience.org>
2020-01-08 18:56:54 +0200

    cepheus: Enable updatable apex
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 # Product launched with 9.0
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
