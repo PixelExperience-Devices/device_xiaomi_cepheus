d93f9a39ab0cb5cc736d9cc5b0dfe451397c2dd9
Nicolas Geoffray <ngeoffray@google.com>
2020-02-07 14:02:18 +0000

    cepheus: Update in pinned files.
    Remove or update paths that don't exist.
    
    Bug: 119800099
    Test: boots
    Change-Id: Ib2cea88499cf08274d620d9d85ffcafc31fee868

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,9 +27,6 @@ include vendor/xiaomi/cepheus/BoardConfigVendor.mk
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
