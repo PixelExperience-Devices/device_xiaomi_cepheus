7f0aa608bacbb441bdba08560ed521ce08b67132
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    cepheus: panic to recovery, not bootloader
    Change-Id: I614dae74dfe603fc2e9256b83e2c0dc72c846686

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,6 +122,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=16
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Media
