fc5f180e175b2624f5d6ad4d259035b419a36705
Akash <akashniki@gmail.com>
2020-02-29 18:29:53 +0000

    cepheus: Enable TARGET_INCLUDE_WIFI_EXT
    This uses google's wifi stuff

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_cepheus
