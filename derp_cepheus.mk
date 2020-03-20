642efa8b896af80079e43cf1bce0e9892f133497
Arian <arian.kulmer@web.de>
2020-03-20 15:54:24 +0100

    cepheus: Initial device tree
    Change-Id: I53dc7c3f5c16f547ea2ba568624ceceb8b3f5e38
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

---
+++ derp_cepheus.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from cepheus device
+$(call inherit-product, device/xiaomi/cepheus/device.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Device identifier
+PRODUCT_NAME := derp_cepheus
+PRODUCT_DEVICE := cepheus
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 9
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
