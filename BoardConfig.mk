0715691cfaa64d27c09c38cd3ca3843e84a2d561
Arian <arian.kulmer@web.de>
2020-03-23 21:42:04 +0100

    cepheus: Configure architecture and platform
    Change-Id: I44382e290d6997fcc97f2b879c2b1c299703a572
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,3 +22,28 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Inherit from proprietary files
 include vendor/xiaomi/cepheus/BoardConfigVendor.mk
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo385
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := cepheus
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msmnile
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
