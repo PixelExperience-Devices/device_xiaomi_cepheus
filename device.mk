8b10f46bf31506999946650b8e5a66d60e2280c4
Arian <arian.kulmer@web.de>
2020-04-20 23:16:59 +0200

    cepheus: drm: Build HAL
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I3b0d2752a5955b326f5ae6e64f29c2888bf101c5
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- device.mk
+++ device.mk
@@ -125,6 +125,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libtinyxml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus
