d101516ec81ab57ab5bf4cba6c24fab47619dd0c
Akash <akashniki@gmail.com>
2020-04-30 09:29:40 +0200

    cepheus: Build livedisplay

--- msmnile.mk
+++ msmnile.mk
@@ -253,6 +253,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msmnile
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
