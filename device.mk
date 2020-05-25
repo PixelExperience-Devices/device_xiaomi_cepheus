55bb69693daa87bcd75bad7dfcf7d0153a11b7fb
DarkDampSquib <andrin.geiger1998@gmail.com>
2020-05-25 10:26:18 +0200

    cepheus: Add PixelLiveWallpaper
    For now. Since the one Rom side seems to be bugged.
    
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# PixelLiveWallpaper
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi-libperfmgr \
