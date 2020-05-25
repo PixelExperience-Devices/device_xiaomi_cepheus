cc7c8d68e9fd7aad5f4080e5b33f0bac6295ee72
TheScarastic <warabhishek@gmail.com>
2020-05-25 10:55:56 +0000

    cepheus: fod: add surfaceflinger extension lib
    Change-Id: I51fb56c0a20e3cac83afb46ed6a6e2f967159d18

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# FOD
+TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.xiaomi_cepheus
+
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
