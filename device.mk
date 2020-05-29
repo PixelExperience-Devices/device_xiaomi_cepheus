9937899227d265dc6e9133c88b779b353c6f1c40
Yan Wang <yawanng@google.com>
2020-05-29 03:00:19 +0000

    cepheus: iorap: Turn pinner on for camera
    Bug: 157667363
    Test: Check the pinner in dumpsys
    Change-Id: I6313e4c7a0ad0e8236390d773f29b2109a82c615
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/cepheus/cepheus-vendor.mk)
 
+include build/make/target/product/iorap_large_memory_config.mk
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
