3484642c60ab0cf4655525ee626a0c26aec80d5c
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    cepheus: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -119,6 +119,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor
 
+PRODUCT_PACKAGES += \
+    libshim_megvii
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
