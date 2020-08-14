MasterAwesome/android_device_xiaomi_laurel_sprout

cepheus: Add assertions on modem version
 * Based off android_device_xiaomi_msm8996-common

Change-Id: Ib578c77492cc3060915b12fad10d023b3ab38875

recovery/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_xiaomi
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_STATIC_LIBRARY)

PixelExperience-Devices/device_xiaomi_cepheus

Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

recovery/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_xiaomi
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_STATIC_LIBRARY)