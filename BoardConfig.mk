901e34af082e2218fa9795da0b73e338cf06779c
EcrosoftXiao <xy19990903@gmail.com>
2020-08-18 16:16:39 +0800

    cepheus: Add support for alipay and wechat fingerprint payment
    Change-Id: I8dce4c82ca924208a65941b2a6efe4ecedda1d6a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,7 @@ TARGET_SEPOLICY_DIR := msmnile
 include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
--- device.mk
+++ device.mk
@@ -174,6 +174,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
