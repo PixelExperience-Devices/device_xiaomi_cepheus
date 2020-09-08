755ad0608c6190d76ff2ac2b09f618458eeb1f84
EcrosoftXiao <xy19990903@gmail.com>
2020-09-08 17:16:54 +0800

    cepheus: Overlay: Make smoothen cutout optional in developer options
    - closer to MIUI
    - smaller default cutout style
    - move old cutout style to developer settings
    
    Signed-off-by: EcrosoftXiao <xy19990903@gmail.com>
    Change-Id: I329c70e17dced723f0093813cd0302d554353162

--- device.mk
+++ device.mk
@@ -350,6 +350,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Smoothen cutout
+PRODUCT_PACKAGES += \
+    SmoothCutoutOverlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
