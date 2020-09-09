d6cae3e8daf2d17ce835af95b67175f8271d40b8
Lucchetto <lucchetto.tie@live.com>
2020-09-09 23:56:02 +0200

    zippo: build vendor libhwbinder
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -91,6 +91,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti
