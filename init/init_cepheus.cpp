c07472781d26246a3aa2db8324283c9dc7aedd93
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-15 00:54:16 +0300

    cepheus: init: Replace property_set() with android::base::SetProperty()
    * follow up https://android.googlesource.com/platform/system/core/+/c88d8f93cfaa8cbf368a0fa94e8bc84bc55c3ece

--- init/init_cepheus.cpp
+++ init/init_cepheus.cpp
@@ -32,11 +32,10 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
 
 using android::base::GetProperty;
-using android::init::property_set;
+using android::base::SetProperty;
 
 
 void property_override(char const prop[], char const value[])
