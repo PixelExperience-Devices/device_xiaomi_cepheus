d9bac0d304631ac57be9013b6bcc24ac26abbc86
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-09-28 03:48:37 +0000

    cepheus: Enable v1.3 widevine drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420

--- device.mk
+++ device.mk
@@ -148,7 +148,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.3-service.widevine
 
 # Fingerprint
 PRODUCT_PACKAGES += \
