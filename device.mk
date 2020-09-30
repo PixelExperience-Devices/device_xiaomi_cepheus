88d30f3821e2e51b5dd35525f096287aaca8b494
Lucchetto <lucchetto.tie@live.com>
2020-09-30 10:57:18 +0200

    cepheus: switch to OSS vibrator hal
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -438,6 +438,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator@1.2-service
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
