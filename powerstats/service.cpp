9e2b3b0231f9b7dfba82e62687daaa985cc0ffab
EcrosoftXiao <xy19990903@gmail.com>
2020-10-18 09:00:37 +0800

    cepheus: power.stats: rebrand to xiaomi
    Signed-off-by: EcrosoftXiao <xy19990903@gmail.com>

--- powerstats/service.cpp
+++ powerstats/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power.stats@1.0-service.pixel"
+#define LOG_TAG "android.hardware.power.stats@1.0-service.xiaomi"
 
 #include <android/log.h>
 #include <binder/IPCThreadState.h>
