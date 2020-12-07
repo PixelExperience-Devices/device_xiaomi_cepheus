9321d745933a32042072ec247a7ac984a7f3dff7
Rahul Shahare <rshaha@codeaurora.org>
2020-12-07 21:31:10 +0530

    cepheus: Add kpti=off to boot command line
    This will OFF kernel page table isolation.
    
    Notes from Kuba Wojciechowski <nullbytepl@gmail.com> in https://github.com/tadiphone-caf/msm-4.14/commit/86bb20a8325d32ac678573c86874f36ec4575ee2:
    As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not required".
    It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Idd36c359d4955701e9ee3ee27a58177a8d622c86
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=16
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += kpti=off
 
 # Media
 TARGET_USES_ION := true
