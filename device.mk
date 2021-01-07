c7f60f13aac2dc0750298d6d36620d7e9b509b08
Evasi0n Xiao <xy19990903@gmail.com>
2021-01-07 17:52:49 +0800

    [SQUASH]cepheus: Nuke LiveDisplay HAL
    Revert "cepheus: livedisplay: remove libhidltransport dependency"
    
    This reverts commit fd2e15db23453bd41da7f0d888e7e18023b2da2f.
    
    Revert "cepheus: livedisplay: Fix isEnabled return"
    
    This reverts commit ea93ea78ed214062bb60964ac35adbc17960ff4c.
    
    Revert "cepheus: Implement SunlightEnhancement LiveDisplay HAL"
    
    This reverts commit bca66f828c976381d960f6fe136821208ad5744e.

--- device.mk
+++ device.mk
@@ -256,11 +256,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.cepheus
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.cepheus
-
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
