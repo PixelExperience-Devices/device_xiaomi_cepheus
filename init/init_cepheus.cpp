74ace3204fa5259e772a1016fb61708a2d44a45d
Evasi0n Xiao <xy19990903@gmail.com>
2021-01-17 10:32:34 +0800

    cepheus: Update from China Beta 21.1.14

--- init/init_cepheus.cpp
+++ init/init_cepheus.cpp
@@ -56,7 +56,7 @@ void property_override_dual(char const system_prop[],
 
 void vendor_load_properties() {
     // fingerprint
-    property_override("ro.build.description", "cepheus-user 10 QKQ1.190825.002 V12.0.3.0.QFAEUXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
+    property_override("ro.build.description", "cepheus-user 11 RKQ1.200826.002 21.1.14 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys");
     property_override("ro.boot.verifiedbootstate", "green");
 }
