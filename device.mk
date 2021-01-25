b4aae242819ad439c2b4d16812811ed1d36c52b8
EcrosoftXiao <xy19990903@gmail.com>
2021-01-25 14:17:43 +0800

    cepheus: Switch to QTI thermal HAL

--- device.mk
+++ device.mk
@@ -437,11 +437,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
-PRODUCT_PACKAGES += \
-    thermal.msmnile
+    android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
