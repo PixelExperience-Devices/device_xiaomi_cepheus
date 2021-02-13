f875d3597c47999599ec3577d7f68a0907e855ba
EcrosoftXiao <xy19990903@gmail.com>
2021-02-13 18:30:22 +0800

    cepheus: Add PE specific overlay stuff

--- device.mk
+++ device.mk
@@ -332,7 +332,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pe
 
 # Power
 PRODUCT_PACKAGES += \
