e8f673cecb4fc007e6467088364313aa48f3739c
EcrosoftXiao <xy19990903@gmail.com>
2021-02-19 18:31:20 +0800

    cepheus: Drop unused permissions

--- device.mk
+++ device.mk
@@ -353,7 +353,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/permissions/system_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/system_privapp-permissions-qti.xml
 
 # RCS
 PRODUCT_PACKAGES += \
