8ac72e15427490a31102c6d093eae2cf0ea0cbf4
EcrosoftXiao <xy19990903@gmail.com>
2021-03-10 15:42:06 +0800

    cepheus: Bring back LiveDisplay HAL
    This reverts commit 5ebfa970a1d8ff131e0a3c0713d4a0cae3dcb224.

--- device.mk
+++ device.mk
@@ -277,6 +277,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.cepheus
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.cepheus
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
