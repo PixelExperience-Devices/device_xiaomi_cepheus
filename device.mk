90f1ae7a0f5af9a2a608499374fb8cab7ecdcbe7
EcrosoftXiao <xy19990903@gmail.com>
2021-03-27 08:56:15 +0800

    Revert "cepheus: Default blurs to on on coral and flame"
    This reverts commit 4f2600593425d068437b261c3e013237d55b0a99.

--- device.mk
+++ device.mk
@@ -179,9 +179,10 @@ PRODUCT_PACKAGES += \
 # Enable incremental FS feature
 PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
 
-# Enable blurs
+# Enable blurs, hidden under dev option
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
 # Fingerprint
