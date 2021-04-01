7536b8756378c2db17f874a6c9c7ea31d54f8f7f
EcrosoftXiao <xy19990903@gmail.com>
2021-04-01 08:08:55 +0800

    Revert "cepheus: Provide shim for libcameraservice.so"
    This reverts commit 943d502cdb9120f181eb54a38275832c0a8c81d9.

--- device.mk
+++ device.mk
@@ -121,8 +121,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor
 
 PRODUCT_PACKAGES += \
-    libshim_megvii \
-    libmedia_jni_shim
+    libshim_megvii
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
