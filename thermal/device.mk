b16fb498c4bdf918043a577c771f30fb466e3d9a
EcrosoftXiao <xy19990903@gmail.com>
2021-04-06 17:04:58 +0800

    cepheus: Import pixel thermal hal
    * imported from https://android.googlesource.com/platform/hardware/google/pixel/+/refs/tags/android-11.0.0_r35
    * commit 7c8556bf9a436ff0eaf4601ed74e86aa05f7afbf

---
+++ thermal/device.mk
@@ -0,0 +1,10 @@
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.pixel \
+
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PACKAGES += \
+    thermal_logd
+endif
+
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/thermal
