89177d437ddb7b363df7a734aaecec8462c02e3d
EcrosoftXiao <xy19990903@gmail.com>
2021-04-15 18:28:48 +0800

    cepheus: No need to ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,6 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=16
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Media
 TARGET_USES_ION := true
