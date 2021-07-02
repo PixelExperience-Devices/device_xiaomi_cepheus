8d187f142454aa0ffc2c7070c089d627934c47a8
Arian <arian.kulmer@web.de>
2021-07-02 12:30:21 +0200

    cepheus: parts: Enable doze_status on screen off
    * This allows to enter the doze state faster when unlocking from lockscreen
    
    Change-Id: I1900436c974a605555275ef67b283ead2cf720b5

--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -57,6 +57,11 @@ public final class DozeUtils {
     protected static final String DOZE_MODE_HBM = "1";
     protected static final String DOZE_MODE_LBM = "0";
 
+    private static final String DOZE_STATUS_PATH =
+            "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/doze_status";
+    protected static final String DOZE_STATUS_ENABLED = "1";
+    protected static final String DOZE_STATUS_DISABLED = "0";
+
     protected static final String DOZE_BRIGHTNESS_LBM = "0";
     protected static final String DOZE_BRIGHTNESS_HBM = "1";
     protected static final String DOZE_BRIGHTNESS_AUTO = "2";
@@ -78,9 +83,7 @@ public final class DozeUtils {
     }
 
     public static void checkDozeService(Context context) {
-        if (isDozeEnabled(context)
-                && (!isAlwaysOnEnabled(context) || isDozeAutoBrightnessEnabled(context))
-                && sensorsEnabled(context)) {
+        if (isDozeEnabled(context) && (isAlwaysOnEnabled(context) || sensorsEnabled(context))) {
             startService(context);
         } else {
             stopService(context);
@@ -140,6 +143,10 @@ public final class DozeUtils {
 
     protected static boolean setDozeMode(String value) {
         return FileUtils.writeLine(DOZE_MODE_PATH, value);
+    }
+
+    protected static boolean setDozeStatus(String value) {
+        return FileUtils.writeLine(DOZE_STATUS_PATH, value);
     }
 
     protected static boolean isDozeAutoBrightnessEnabled(Context context) {
