aff4e116e58441e3fc3f92b3d10799720f2ecd51
Tim Zimmermann <tim@linux4.de>
2021-07-26 21:13:30 +0530

    cepheus: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Change-Id: Icbc828389465f3d4681ca8e4c3adec782540752e
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -17,6 +17,14 @@
 import common
 import re
 
+def FullOTA_InstallBegin(info):
+  input_zip = info.input_zip
+  AddImage(info, "RADIO", input_zip, "super_dummy.img", "/tmp/super_dummy.img");
+  info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
+  info.script.AppendExtra('set_metadata("/tmp/flash_super_dummy.sh", "uid", 0, "gid", 0, "mode", 0755);')
+  info.script.AppendExtra('run_program("/tmp/flash_super_dummy.sh");')
+  return
+
 def FullOTA_InstallEnd(info):
   input_zip = info.input_zip
   OTA_UpdateFirmware(info)
@@ -61,8 +69,8 @@ def OTA_UpdateFirmware(info):
   info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config");')
   info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_configbak");')
 
-def AddImage(info, input_zip, basename, dest):
-  path = "IMAGES/" + basename
+def AddImage(info, dir, input_zip, basename, dest):
+  path = dir + "/" + basename
   if path not in input_zip.namelist():
     return
 
@@ -73,6 +81,6 @@ def AddImage(info, input_zip, basename, dest):
 
 def OTA_InstallEnd(info, input_zip):
   info.script.Print("Patching firmware images...")
-  AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "IMAGES", input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "IMAGES", input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
