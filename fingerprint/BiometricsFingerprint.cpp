2a3111d1a0278b418a705041528d998aa4e5168c
EcrosoftXiao <xy19990903@gmail.com>
2021-10-21 00:01:49 +0800

    [DNM] cepheus: fingerprint: Do not handle touchpanel fod status on Finger{Up,down} event
    This fixes screen off fod

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -470,14 +470,12 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
-    set(FOD_STATUS_PATH, FOD_STATUS_ON);
     set(DIMLAYER_HBM_PATH, DIMLAYER_HBM_ON);
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
     set(DIMLAYER_HBM_PATH, DIMLAYER_HBM_OFF);
-    set(FOD_STATUS_PATH, FOD_STATUS_OFF);
     return Void();
 }
 
