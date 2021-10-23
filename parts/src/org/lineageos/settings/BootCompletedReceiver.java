02d76026737ced2258c1a8bc74b791fb761bd0aa
Elluzion <dyrex2004@gmail.com>
2021-10-23 17:32:38 +0000

    cepheus: parts: Nuke per-app thermals
    I consider these as kind of useless bloat, so I'll remove them.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -29,7 +29,6 @@ import androidx.preference.PreferenceManager;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.utils.FileUtils;
-import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -47,7 +46,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         PendingResult pendingResult = goAsync();
         DiracUtils.initialize(context);
         DozeUtils.onBootCompleted(context);
-        ThermalUtils.startService(context);
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
