c8eb3398fa7630e6d2cf0181f4d12bdbd5fd1494
EcrosoftXiao <xy19990903@gmail.com>
2021-11-29 22:32:01 +0800

    Revert "[DNM] cepheus: Set SELinux to permissive"
    This reverts commit da927a2c68d2910009c631058b06b5204aa2fc6e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/cepheus
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
