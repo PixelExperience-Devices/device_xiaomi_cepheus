ccabbc9042173858729a88453b447256f8f7c817
EcrosoftXiao <xy19990903@gmail.com>
2021-11-30 17:57:35 +0800

    cepheus: Track xiaomi sdm 845 common vendor and move to common IFAA impl

--- device.mk
+++ device.mk
@@ -234,12 +234,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
-PRODUCT_PACKAGES += \
-    IFAAService \
-    org.ifaa.android.manager
-
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+include vendor/xiaomi/sdm845-common-extra/ifaa.mk
 
 # Init scripts
 PRODUCT_COPY_FILES += \
