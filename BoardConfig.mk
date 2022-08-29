63959f0f9e9aca6a5b38ae2e3fa7acb66f2ecde4
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    cepheus: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,7 +183,7 @@ VENDOR_SECURITY_PATCH := 2021-10-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmnile
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
