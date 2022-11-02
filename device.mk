3762c3a27a6ac72c8f2e018a9e6fed482ca1ab90
EcrosoftXiao <xy19990903@gmail.com>
2022-11-02 16:55:01 +0800

    cepheus: Switch to OSS IFAAService

--- device.mk
+++ device.mk
@@ -239,10 +239,6 @@ PRODUCT_COPY_FILES += \
 # IFAA manager
 PRODUCT_PACKAGES += \
     IFAAService \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
 
 # Init scripts
 PRODUCT_COPY_FILES += \
