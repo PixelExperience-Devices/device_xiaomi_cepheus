e780f514955897ce9dc6b42f51793a2e2d3b2396
EcrosoftXiao <xy19990903@gmail.com>
2023-02-20 00:57:03 +0800

    cepheus: Switch to udfps sensor impl

--- device.mk
+++ device.mk
@@ -438,6 +438,12 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
+PRODUCT_PACKAGES += \
+    sensors.udfps
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
